84d0fca358fa104d05dd0280aa91e413924d3c0c
Baris Celebi <76443957+BarisClb@users.noreply.github.com>
2022-02-26 12:17:07 +0300

    Update scripts.js

--- Fun_Projects/Calculator v1 (Plain HTML, CSS, JS)/scripts.js
+++ Fun_Projects/Calculator v1 (Plain HTML, CSS, JS)/scripts.js
@@ -4,116 +4,66 @@ const screen2 = document.querySelector(".screen-2");
 const screen3 = document.querySelector(".operation-screen");
 const operations = document.querySelector(".operations-pad");
 
-
-
-
-
 let num1 = "";
 let num2 = "";
 let operation = "";
-
 
 eventListeners();
 
 function eventListeners() {
     keypad.addEventListener("click", addNum);
     operations.addEventListener("click", operateOrEqual);
-
-
-
 }
-
-// function getNum(e) {
-    
-//     console.log(Number(e.target.id));
-// }
-
-
 
 function addNum(e) {
     num1 += Number(e.target.id);
     screen1.textContent = num1;
-
-
-    console.log(num1);
 }
 
-
 function operateOrEqual(e) {
-
     if (e.target.id == "=") {
         equals(e)
     }
     else {
         operating(e)
     }
-
 }
 
-
-
 function operating(e) {
-    
     if (num1 === "") {
-        
     }
-
     else if (operation === "") {
-
         num2 = num1;
         num1 = "";
+        
         screen1.textContent = num1;
         screen2.textContent = num2;
         
         operation += e.target.id;
         screen3.textContent = e.target.id;
-
     }
-
     else {
-
         operation = e.target.id;
         screen3.textContent = e.target.id;
-
-
     }
-
-
-
-
 }
-
 
 function equals(e) {
     let answer = "";
 
     if (operation == "+") {
-
         answer = Number(num2) + Number(num1);
-
+    }
+    else if (operation == "-") {
+        answer = Number(num2) - Number(num1);
+    }
+    else if (operation == "*") {
+        answer = Number(num2) * Number(num1);
+    }
+    else {
+        answer = Number(num2) / Number(num1);
     }
     
-    else if (operation == "-") {
-
-        answer = Number(num2) - Number(num1);
-
-
-    }
-
-    else if (operation == "*") {
-
-        answer = Number(num2) * Number(num1);
-
-
-    }
-
-    else {
-
-        answer = Number(num2) / Number(num1);
-
-
-    }
-
     num1 = answer;
     screen1.textContent = num1;
 
@@ -122,11 +72,7 @@ function equals(e) {
 
     operation = "";
     screen3.textContent = operation;
-
-
 }
-
-
 
 window.addEventListener("click",showme);
 
